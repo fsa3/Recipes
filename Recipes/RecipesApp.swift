36aeea24e42ac2f242ebcc6c89cac0bc1941ba4b
Fannar Steinn <fannarsteinn@hotmail.com>
2021-11-01 13:39:37 +0100

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Fannar Steinn Aðalsteinsson on 1.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
