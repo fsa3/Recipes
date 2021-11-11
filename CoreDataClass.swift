ae38adf3f801dc1fecb96b34310563ad05e1188b
Tatjana Kraemer <tatjana.kraemer26@web.de>
2021-11-11 15:19:03 +0100

    Core Data modified

---
+++ CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataClass.swift
+//  Recipes
+//
+//  Created by Tatjana Krämer on 11.11.21.
+//
+
+import Foundation
+import CoreData
+
+
+public class Recipies: NSManagedObject {
+
+}
