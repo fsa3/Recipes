4b56c44b07c8f4d00dda1476afccc2a3796a7bcf
Tatjana Kraemer <tatjana.kraemer26@web.de>
2021-11-15 14:53:44 +0100

    Changes on Persistence file

--- Recipes/CoreDataModel/Persistence.swift
+++ Recipes/CoreDataModel/Persistence.swift
@@ -31,7 +31,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "CoreDataTest")
+        container = NSPersistentContainer(name: "CoreDataModel")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
